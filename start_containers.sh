7f66f2b6cb702a8b87164d4a5ac1e6c04cf93995
franjaviersans <franjaviersans@gmail.com>
2018-02-12 10:40:05 -0400

    Now you can set as many containers as you like with the scripts

--- start_containers.sh
+++ start_containers.sh
@@ -1,6 +1,30 @@
 #!/bin/bash -v
-docker network create mynet
-docker run -d --name db1 --net mynet -p 27017:27017 -v $(pwd)/data1:/data/db mongo
-docker run -d --name db2 --net mynet -p 27018:27017 -v $(pwd)/data2:/data/db mongo
-docker run -d --name seleniumserver1 --net mynet -p 3838:80  -p 3333:4444 -v $(pwd):/usr/src/app --shm-size=2g -e MAIN_APP_FILE=./docker_images/selenium_sinatra_firefox/serverRest.rb -e DBDNS=db1 selenium_sinatra_firefox
-docker run -d --name seleniumserver2 --net mynet -p 2929:80  -p 5555:4444 -v $(pwd):/usr/src/app --shm-size=2g -e MAIN_APP_FILE=./docker_images/selenium_sinatra_firefox/serverRest.rb -e DBDNS=db2 selenium_sinatra_firefox
+if [ $# != 1 ]; then
+	echo "ERROR >>>> You have to set the number of server containers to start"
+else
+	i="0"
+	
+	docker network create mynet
+	#erase servers and databases file
+	rm servers.txt
+	rm databases.txt
+
+	while [ $i -lt $1 ]; do
+		portDB=$[27017+$i]
+		docker run -d --name db$i --net mynet -p $[portDB]:27017 -v $(pwd)/db$i:/data/db$i mongo
+		
+		portSinatra=$[3838+$i]
+		portSelenium=$[3333+$i]
+		
+		docker run -d --name seleniumserver$i --net mynet -p $[portSinatra]:80  -p $[portSelenium]:4444 -v $(pwd):/usr/src/app --shm-size=2g -e MAIN_APP_FILE=./docker_images/selenium_sinatra_firefox/serverRest.rb -e DBDNS=db$i selenium_sinatra_firefox
+
+		#print to servers file
+		echo seleniumserver$i >> "servers.txt"
+		echo db$i >> "databases.txt"
+		
+		i=$[$i+1]
+	done
+
+	#copy the list of servers to the client local folder, so it can access it
+	cp servers.txt ./docker_images/python_client 
+fi
